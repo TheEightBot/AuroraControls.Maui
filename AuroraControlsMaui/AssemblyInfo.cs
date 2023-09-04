1da37593ef26cd83a347003b4404520d9331956a
Michael Stonis <120685+michaelstonis@users.noreply.github.com>
2023-09-03 19:02:49 -0500

    Add AuroraControlsMaui/AssemblyInfo.cs
    - Add using statement for AuroraControls namespace
    - Add XmlnsPrefix attributes for AuroraControls, AuroraControls.Gauges, and AuroraControls.Loading namespaces
    - Add XmlnsDefinition attribute for the AuroraControls namespace

---
+++ AuroraControlsMaui/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using AuroraControls;
+
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix("http://auroracontrols.maui/schemas/controls", "AuroraControls")]
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix("http://auroracontrols.maui/schemas/controls", "AuroraControls.Gauges")]
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix("http://auroracontrols.maui/schemas/controls", "AuroraControls.Loading")]
+[assembly: Microsoft.Maui.Controls.XmlnsDefinition("http://auroracontrols.maui/schemas/controls", "AuroraControls")]
+
+namespace AuroraControls;
