b1788995ba1a83aa923b9e48b9d6016b78a3ad44
Michael Stonis <michael.stonis@eightbot.com>
2025-06-26 14:51:15 -0500

    Adds SvgImageButton control
    Introduces a new SvgImageButton control for displaying SVG images as buttons.
    
    - Implements touch event handling with animation.
    - Supports different background shapes and overlay color.
    - Provides command execution on click.

--- AuroraControlsMaui/VisualEffects/Grayscale.cs
+++ AuroraControlsMaui/VisualEffects/Grayscale.cs
@@ -1,33 +1,32 @@
-﻿namespace AuroraControls.VisualEffects
+﻿namespace AuroraControls.VisualEffects;
+
+public class Grayscale : VisualEffect
 {
-    public class Grayscale : VisualEffect
+    private static readonly float[] _colorMatrix =
+    [
+        0.21f, 0.72f, 0.07f, 0.0f, 0.0f,
+        0.21f, 0.72f, 0.07f, 0.0f, 0.0f,
+        0.21f, 0.72f, 0.07f, 0.0f, 0.0f,
+        0.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+    ];
+
+    private readonly SKColorFilter _colorFilter;
+
+    public Grayscale() => _colorFilter = SKColorFilter.CreateColorMatrix(_colorMatrix);
+
+    public override SKImage ApplyEffect(SKImage image, SKSurface surface, SKImageInfo info, SKRect overrideRect) => InternalApplyEffect(surface, info.Rect);
+
+    public override SKImage ApplyEffect(SKImage image, SKSurface surface, GRBackendRenderTarget info, SKRect overrideRect) => InternalApplyEffect(surface, info.Rect);
+
+    private SKImage InternalApplyEffect(SKSurface surface, SKRect rect)
     {
-        private static readonly float[] _colorMatrix =
-            [
-                0.21f, 0.72f, 0.07f, 0.0f, 0.0f,
-                0.21f, 0.72f, 0.07f, 0.0f, 0.0f,
-                0.21f, 0.72f, 0.07f, 0.0f, 0.0f,
-                0.0f,  0.0f,  0.0f,  1.0f, 0.0f,
-            ];
-
-        private readonly SKColorFilter _colorFilter;
-
-        public Grayscale() => _colorFilter = SKColorFilter.CreateColorMatrix(_colorMatrix);
-
-        public override SKImage ApplyEffect(SKImage image, SKSurface surface, SKImageInfo info, SKRect overrideRect) => InternalApplyEffect(surface, info.Rect);
-
-        public override SKImage ApplyEffect(SKImage image, SKSurface surface, GRBackendRenderTarget info, SKRect overrideRect) => InternalApplyEffect(surface, info.Rect);
-
-        private SKImage InternalApplyEffect(SKSurface surface, SKRect rect)
+        using (var paint = new SKPaint { ColorFilter = _colorFilter, })
+        using (var surfaceImage = surface.Snapshot())
         {
-            using (var paint = new SKPaint { ColorFilter = _colorFilter, })
-            using (var surfaceImage = surface.Snapshot())
-            {
-                surface.Canvas.Clear();
-                surface.Canvas.DrawImage(surfaceImage, rect, paint);
-            }
-
-            return surface.Snapshot();
+            surface.Canvas.Clear();
+            surface.Canvas.DrawImage(surfaceImage, rect, paint);
         }
+
+        return surface.Snapshot();
     }
 }
