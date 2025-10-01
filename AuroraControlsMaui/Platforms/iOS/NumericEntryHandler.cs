264ae7eb3656ad5587a339f7a9d19f92ccdf070a
Michael Stonis <michael.stonis@eightbot.com>
2025-10-01 11:25:11 -0500

    Improves numeric entry on iOS
    Adds a "Done" button to the numeric keyboard on iOS
    
    *   Adds a Done button to the iOS keyboard for numeric entry.
    *   Disables text prediction for numeric entries.
    *   Sets the keyboard type to numeric for numeric entries.

--- AuroraControlsMaui/Platforms/iOS/NumericEntryHandler.cs
+++ AuroraControlsMaui/Platforms/iOS/NumericEntryHandler.cs
@@ -1,15 +1,25 @@
-﻿using Foundation;
+﻿using System.Drawing;
+using Foundation;
 using Microsoft.Maui.Handlers;
 using Microsoft.Maui.Platform;
 using UIKit;
 
 namespace AuroraControls;
 
-public partial class NumericEntryHandler : EntryHandler
+public partial class NumericEntryHandler : EntryHandler, IDisposable
 {
+    private UIBarButtonItem _doneButton;
+
     protected override void ConnectHandler(MauiTextField platformView)
     {
         base.ConnectHandler(platformView);
+
+        var toolbar = new UIToolbar(new RectangleF(0, 0, (float)UIScreen.MainScreen.Bounds.Width, 44)) { BarStyle = UIBarStyle.Default, Translucent = true };
+        _doneButton = new UIBarButtonItem(UIBarButtonSystemItem.Done);
+        _doneButton.Clicked += DoneButtonOnClicked;
+
+        toolbar.Items = [new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace), _doneButton,];
+        platformView.InputAccessoryView = toolbar;
 
         platformView.ShouldChangeCharacters += OnShouldChangeCharacters;
     }
@@ -17,8 +27,17 @@ public partial class NumericEntryHandler : EntryHandler
     protected override void DisconnectHandler(MauiTextField platformView)
     {
         platformView.ShouldChangeCharacters -= OnShouldChangeCharacters;
+        _doneButton.Clicked -= DoneButtonOnClicked;
+
+        PlatformView.InputAccessoryView?.Dispose();
+        PlatformView.InputAccessoryView = null;
 
         base.DisconnectHandler(platformView);
+    }
+
+    private void DoneButtonOnClicked(object? sender, EventArgs e)
+    {
+        this.PlatformView.ResignFirstResponder();
     }
 
     private bool OnShouldChangeCharacters(UITextField textField, NSRange range, string replacementString)
@@ -35,5 +54,19 @@ public partial class NumericEntryHandler : EntryHandler
             originalDest.Substring(0, (int)range.Location) + replacementString + originalDest.Substring((int)range.Location + (int)range.Length);
 
         return IsValid(final, NumericEntryVirtualView.CultureInfo, NumericEntryVirtualView.ValueType);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _doneButton.Dispose();
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
     }
 }
