f6182313ddcf399f2c7597ad8f7fddd4700c2961
AJ Rotolo <aj.rotolo@eightbot.com>
2026-01-28 19:39:09 -0600

    add calendarpicker font mappings

--- AuroraControlsMaui/Platforms/Android/CalendarPickerHandler.cs
+++ AuroraControlsMaui/Platforms/Android/CalendarPickerHandler.cs
@@ -1,4 +1,5 @@
 using Android.App;
+using Microsoft.Maui;
 using Microsoft.Maui.Handlers;
 
 namespace AuroraControls;
@@ -9,6 +10,9 @@ public partial class CalendarPickerHandler : DatePickerHandler
         new(ViewMapper)
         {
             [nameof(CalendarPicker.Date)] = MapDate,
+            [nameof(CalendarPicker.FontSize)] = MapFontSize,
+            [nameof(CalendarPicker.FontFamily)] = MapFontFamily,
+            [nameof(CalendarPicker.FontAttributes)] = MapFontAttributes,
         };
 
     public CalendarPickerHandler()
@@ -36,6 +40,69 @@ public partial class CalendarPickerHandler : DatePickerHandler
     }
 
     public static void MapDate(CalendarPickerHandler handler, CalendarPicker view) => handler.TryShowEmptyState();
+
+    public static void MapFontSize(CalendarPickerHandler handler, CalendarPicker view)
+    {
+        if (handler.PlatformView != null)
+        {
+            ApplyFont(handler, view);
+        }
+    }
+
+    public static void MapFontFamily(CalendarPickerHandler handler, CalendarPicker view)
+    {
+        if (handler.PlatformView != null)
+        {
+            ApplyFont(handler, view);
+        }
+    }
+
+    public static void MapFontAttributes(CalendarPickerHandler handler, CalendarPicker view)
+    {
+        if (handler.PlatformView != null)
+        {
+            ApplyFont(handler, view);
+        }
+    }
+
+    private static void ApplyFont(CalendarPickerHandler handler, CalendarPicker view)
+    {
+        handler.PlatformView.SetTextSize(Android.Util.ComplexUnitType.Sp, (float)view.FontSize);
+
+        var typeface = GetTypeface(view);
+        if (typeface != null)
+        {
+            handler.PlatformView.Typeface = typeface;
+        }
+    }
+
+    private static Android.Graphics.Typeface? GetTypeface(CalendarPicker view)
+    {
+        var fontFamily = view.FontFamily;
+        var fontAttributes = view.FontAttributes;
+
+        if (string.IsNullOrEmpty(fontFamily))
+        {
+            return fontAttributes switch
+            {
+                FontAttributes.Bold => Android.Graphics.Typeface.DefaultBold,
+                FontAttributes.Italic => Android.Graphics.Typeface.Create(Android.Graphics.Typeface.Default, Android.Graphics.TypefaceStyle.Italic),
+                _ => Android.Graphics.Typeface.Default,
+            };
+        }
+
+        var typeface = Android.Graphics.Typeface.Create(fontFamily, Android.Graphics.TypefaceStyle.Normal);
+        if (fontAttributes == FontAttributes.Bold)
+        {
+            typeface = Android.Graphics.Typeface.Create(typeface, Android.Graphics.TypefaceStyle.Bold);
+        }
+        else if (fontAttributes == FontAttributes.Italic)
+        {
+            typeface = Android.Graphics.Typeface.Create(typeface, Android.Graphics.TypefaceStyle.Italic);
+        }
+
+        return typeface;
+    }
 
     public void TryShowEmptyState()
     {
